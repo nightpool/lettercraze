5459343bc6055e0eca6c9da0683c5c5820e168d1
evan <eg1290@gmail.com>
2016-12-05 16:05:09 -0500

    Create Hamburger Menu component

--- src/edu/wpi/zirconium/lettercraze/builder/controllers/MenuController.java
+++ src/edu/wpi/zirconium/lettercraze/builder/controllers/MenuController.java
@@ -1,12 +1,19 @@
 package edu.wpi.zirconium.lettercraze.builder.controllers;
 
 import edu.wpi.zirconium.lettercraze.builder.LetterCrazeBuilder;
-import javafx.application.Platform;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -18,9 +25,6 @@ public class MenuController implements Initializable {
     @FXML private StackPane collectionsButton;
     @FXML private StackPane achievementsButton;
 
-    @FXML private Pane hamburger;
-    @FXML private Pane hamburgerMenu;
-
     @FXML private Pane root;
 
     @Override
@@ -31,26 +35,23 @@ public class MenuController implements Initializable {
         collectionsButton.setOnMouseClicked(this::showNotImplemented);
         achievementsButton.setOnMouseClicked(this::showNotImplemented);
 
-        hamburger.setOnMouseClicked(this::openHamburger);
     }
 
-    private void showNotImplemented(MouseEvent mouseEvent) {
-
-    }
-
-    private void openHamburger(MouseEvent mouseEvent) {
-        if (!hamburgerMenu.isVisible()) {
-            hamburgerMenu.setVisible(true);
-            hamburger.getStyleClass().add("active");
-
-            Platform.runLater(() -> {
-                root.setOnMouseClicked(me -> {
-                    hamburger.getStyleClass().remove("active");
-                    hamburgerMenu.setVisible(false);
-                    root.setOnMouseClicked(null);
-                });
-            });
-        }
+    @FXML private void showNotImplemented(Event mouseEvent) {
+        final Stage dialog = new Stage(StageStyle.UNDECORATED);
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.initOwner(root.getScene().getWindow());
+        VBox dialogVbox = new VBox(20);
+        dialogVbox.setStyle(
+            "-fx-background-color: white; -fx-border-color: #434343; -fx-border-width: 2px"
+        );
+        Text text = new Text("This feature is not yet implemented.");
+        text.setFont(Font.font("Open Sans", 20));
+        dialogVbox.getChildren().add(text);
+        Scene dialogScene = new Scene(dialogVbox);
+        dialog.setScene(dialogScene);
+        dialog.sizeToScene();
+        dialog.show();
     }
 
     private void createNewLevel(MouseEvent mouseEvent) {
