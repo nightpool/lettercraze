3ca4fd156795cb00ff483682f815c0ab5bb6eb8a
Chris Bove <bove.3123@gmail.com>
2016-12-07 11:12:48 -0500

    some additional structure for the player

--- src/edu/wpi/zirconium/lettercraze/player/LetterCrazePlayer.java
+++ src/edu/wpi/zirconium/lettercraze/player/LetterCrazePlayer.java
@@ -1,6 +1,8 @@
 package edu.wpi.zirconium.lettercraze.player;
 
+import java.util.ArrayList;
 import com.sun.javafx.application.LauncherImpl;
+import edu.wpi.zirconium.lettercraze.entities.LevelPackData;
 import edu.wpi.zirconium.lettercraze.views.SplashScreen;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -8,10 +10,32 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Main application class for the letter craze player.
+ * <p>
+ * @author Zirconium
+ *
+ */
 public class LetterCrazePlayer extends Application {
-
+	/** Holder for the Java FX stage.  */
     static private Stage stage;
-
+    
+    // TODO - achievements
+    // Achievement achievements...
+    
+    /** Array that holds the level packs, each one containing all the levels for a game. */ 
+    protected ArrayList<LevelPack> levelPacks = new ArrayList<LevelPack>();
+    
+    /** The current round of the game. */
+    Round currentRound;
+    
+    /**
+     * Default constructor.
+     */
+    LetterCrazePlayer() {
+    	
+    }
+    
     @Override
     public void init() {
         try {
@@ -40,6 +64,36 @@ public class LetterCrazePlayer extends Application {
     public static void showPlayerLevelScreen() throws Exception {
         Parent builder = FXMLLoader.load(LetterCrazePlayer.class.getResource("views/Level.fxml"));
         stage.setScene(new Scene(builder, 1024, 712));
+    }
+    
+// TODO achievement data stuff
+    
+    /**
+     * Loads the LavelPack
+     * @param file
+     * @return
+     */
+    public LevelPackData loadData(String file){
+    	// TODO 
+    	return new LevelPackData;
+    }
+    
+    /**
+     * Starts the round from the given level.
+     * @param level to start round with
+     * @return true if no errors occurred
+     */
+    public boolean startRound(Level level){
+    	currentRound = new Round(level);
+    	// TODO - is this where the check happens if a locked level can be played?
+    	return true;
+    }
+    
+    /**
+     * Updates the stats for the current round's progress.
+     */
+    public void updateStatsFromCurrentRound() {
+    	// TODO no idea.
     }
 
     public static void main(String[] args) {
