f14ac5508cc07ca7e6db7627f931ff8d482c8ee3
Chris Bove <bove.3123@gmail.com>
2016-12-08 13:24:15 -0500

    setup unit test cases with JUnit 4 and build path (#16)
    * setup unit test cases with JUnit 4 and build path
    
    * make the constructor of a level public
    
    * rename package to match that in src
    
    * now that we're in the same package, don't need a getter

---
+++ test/edu/wpi/zirconium/lettercraze/entities/TestRound.java
@@ -0,0 +1,97 @@
+/**
+ * 
+ */
+package edu.wpi.zirconium.lettercraze.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.wpi.zirconium.lettercraze.entities.Round;
+import edu.wpi.zirconium.lettercraze.entities.Level;
+
+/**
+ * @author Chris B
+ *
+ */
+public class TestRound {
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Round#Round(edu.wpi.zirconium.lettercraze.entities.Level)}.
+	 */
+	@Test
+	public void testRound() {
+		Level level = new Level("1");
+		Round round = new Round(level);
+		assertEquals(0, round.getScore());
+		assertEquals(0, round.getTime());
+		assertEquals(0, round.getNumWordsFound());
+		assertFalse(round.isOver());
+		assertEquals(level, round.level);
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Round#reset()}.
+	 */
+	@Test
+	public void testReset() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Round#doMove()}.
+	 */
+	@Test
+	public void testDoMove() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Round#undoMove()}.
+	 */
+	@Test
+	public void testUndoMove() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Round#getScore()}.
+	 */
+	@Test
+	public void testGetScore() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Round#getTime()}.
+	 */
+	@Test
+	public void testGetTime() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Round#getNumWordsFound()}.
+	 */
+	@Test
+	public void testGetNumWordsFound() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Round#isOver()}.
+	 */
+	@Test
+	public void testIsOver() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Round#incrementTime()}.
+	 */
+	@Test
+	public void testIncrementTime() {
+		fail("Not yet implemented");
+	}
+
+}
