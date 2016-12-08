386328e45a6765ff9fc30f038ad0f18ec5cbd6d5
Chris Bove <bove.3123@gmail.com>
2016-12-08 12:23:08 -0500

    setup unit test cases with JUnit 4 and build path

---
+++ test/edu/wpi/zirconium/lettercraze/test/entities/TestRound.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+package edu.wpi.zirconium.lettercraze.test.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Chris B
+ *
+ */
+public class TestRound {
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Round#Round(edu.wpi.zirconium.lettercraze.entities.Level)}.
+	 */
+	@Test
+	public void testRound() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Round#reset()}.
+	 */
+	@Test
+	public void testReset() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Round#doMove()}.
+	 */
+	@Test
+	public void testDoMove() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Round#undoMove()}.
+	 */
+	@Test
+	public void testUndoMove() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Round#getScore()}.
+	 */
+	@Test
+	public void testGetScore() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Round#getTime()}.
+	 */
+	@Test
+	public void testGetTime() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Round#getNumWordsFound()}.
+	 */
+	@Test
+	public void testGetNumWordsFound() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Round#isOver()}.
+	 */
+	@Test
+	public void testIsOver() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Round#incrementTime()}.
+	 */
+	@Test
+	public void testIncrementTime() {
+		fail("Not yet implemented");
+	}
+
+}
