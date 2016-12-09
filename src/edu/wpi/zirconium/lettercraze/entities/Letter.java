d056d1afdcbb092f1adcfbe54c78a740f5325049
Chris Bove <bove.3123@gmail.com>
2016-12-09 10:43:10 -0500

    working random letters based on proper probablity distribution

--- src/edu/wpi/zirconium/lettercraze/entities/Letter.java
+++ src/edu/wpi/zirconium/lettercraze/entities/Letter.java
@@ -28,8 +28,68 @@ public class Letter {
         return character;
     }
 
+    /**
+     * This generates a new random letter based on probability distribution of letters
+     * for the English language.
+     * @return new letter based on proper distribution.
+     */
     public static Letter random() {
-        Random r = new Random();
-        return new Letter(String.valueOf((char)(r.nextInt(26) + 'a')), 0);
+    	double rand = new Random().nextDouble(); // generate number between 0 and 1
+    	String letter;
+    	// get ready for the cascade of doom...
+    	if (rand <= 0.12702)
+    		letter = "E";
+    	else if (rand <= 0.21758)
+    		letter = "T";
+    	else if (rand <= 0.29925)
+    		letter = "A";
+    	else if (rand <= 0.37432)
+    		letter = "O";
+    	else if (rand <= 0.44398)
+    		letter = "I";
+    	else if (rand <= 0.51147)
+    		letter = "N";
+    	else if (rand <= 0.57474)
+    		letter = "S";
+    	else if (rand <= 0.63568)
+    		letter = "H";
+    	else if (rand <= 0.69555)
+    		letter = "R";
+    	else if (rand <= 0.73808)
+    		letter = "D";
+    	else if (rand <= 0.77833)
+    		letter = "L";
+    	else if (rand <= 0.80615)
+    		letter = "C";
+    	else if (rand <= 0.83373)
+    		letter = "U";
+    	else if (rand <= 0.85779)
+    		letter = "M";
+    	else if (rand <= 0.88139)
+    		letter = "W";
+    	else if (rand <= 0.90367)
+    		letter = "F";
+    	else if (rand <= 0.92382)
+    		letter = "G";
+    	else if (rand <= 0.94356)
+    		letter = "Y";
+    	else if (rand <= 0.96285)
+    		letter = "P";
+    	else if (rand <= 0.97777)
+    		letter = "B";
+    	else if (rand <= 0.98755)
+    		letter = "V";
+    	else if (rand <= 0.99527)
+    		letter = "K";
+    	else if (rand <= 0.99680)
+    		letter = "J";
+    	else if (rand <= 0.99830)
+    		letter = "X";
+    	else if (rand <= 0.99925)
+    		letter = "Qu";
+    	else
+    		letter = "Z";
+    	
+        return new Letter(letter, 0);
     }
 }
