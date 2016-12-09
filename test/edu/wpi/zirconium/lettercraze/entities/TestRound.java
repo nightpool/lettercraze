fabeeae16afbe0fcc98d5c166d6951ffd5f5de0a
Chris Bove <bove.3123@gmail.com>
2016-12-08 22:41:38 -0500

    refactor after changes on level, tile, move, and round

--- test/edu/wpi/zirconium/lettercraze/entities/TestRound.java
+++ test/edu/wpi/zirconium/lettercraze/entities/TestRound.java
@@ -34,7 +34,7 @@ public class TestRound {
 	 */
 	@Test
 	public void testReset() {
-		Level level = new Level("1");
+		Level level = new Level(6, "1");
 		Round round = new Round(level);
 		assertEquals(0, round.getTime());
 		round.incrementTime();
@@ -50,15 +50,15 @@ public class TestRound {
 	 */
 	@Test
 	public void testDoMove() {
-		Level level = new Level("1");
+		Level level = new Level(6, "1");
 		Round round = new Round(level);
-		Tile t1 = new Tile(new Letter("a",2),new Point(0, 0));
-		Tile t2 = new Tile(new Letter("c",3),new Point(1, 0));
-		Tile t3 = new Tile(new Letter("t",4),new Point(2, 0));
-		round.moveInProgress.addTile(t1);
-		round.moveInProgress.addTile(t2);
-		round.moveInProgress.addTile(t3);
-		assertTrue(round.doMove());
+		Tile t1 = new Tile(new Point(0, 0),new Letter("a",2));
+		Tile t2 = new Tile(new Point(1, 0),new Letter("c",3));
+		Tile t3 = new Tile(new Point(2, 0),new Letter("t",4));
+		round.moveInProgress.get().addTile(t1);
+		round.moveInProgress.get().addTile(t2);
+		round.moveInProgress.get().addTile(t3);
+		assertTrue(round.submitMove());
 		assertEquals(1, round.getNumWordsFound());
 	}
 
@@ -67,15 +67,15 @@ public class TestRound {
 	 */
 	@Test
 	public void testUndoMove() {
-		Level level = new Level("1");
+		Level level = new Level(6, "1");
 		Round round = new Round(level);
-		Tile t1 = new Tile(new Letter("a",2),new Point(0, 0));
-		Tile t2 = new Tile(new Letter("c",3),new Point(1, 0));
-		Tile t3 = new Tile(new Letter("t",4),new Point(2, 0));
-		round.moveInProgress.addTile(t1);
-		round.moveInProgress.addTile(t2);
-		round.moveInProgress.addTile(t3);
-		assertTrue(round.doMove());
+		Tile t1 = new Tile(new Point(0, 0),new Letter("a",2));
+		Tile t2 = new Tile(new Point(1, 0),new Letter("c",3));
+		Tile t3 = new Tile(new Point(2, 0),new Letter("t",4));
+		round.moveInProgress.get().addTile(t1);
+		round.moveInProgress.get().addTile(t2);
+		round.moveInProgress.get().addTile(t3);
+		assertTrue(round.submitMove());
 		assertEquals(1, round.getNumWordsFound());
 		assertTrue(round.undoMove());
 		assertEquals(0, round.getNumWordsFound());
@@ -86,17 +86,17 @@ public class TestRound {
 	 */
 	@Test
 	public void testUndoUncompletedMove() {
-		Level level = new Level("1");
+		Level level = new Level(6, "1");
 		Round round = new Round(level);
-		Tile t1 = new Tile(new Letter("a",2),new Point(0, 0));
-		Tile t2 = new Tile(new Letter("c",3),new Point(1, 0));
-		Tile t3 = new Tile(new Letter("t",4),new Point(2, 0));
-		round.moveInProgress.addTile(t1);
-		round.moveInProgress.addTile(t2);
-		round.moveInProgress.addTile(t3);
+		Tile t1 = new Tile(new Point(0, 0),new Letter("a",2));
+		Tile t2 = new Tile(new Point(1, 0),new Letter("c",3));
+		Tile t3 = new Tile(new Point(2, 0),new Letter("t",4));
+		round.moveInProgress.get().addTile(t1);
+		round.moveInProgress.get().addTile(t2);
+		round.moveInProgress.get().addTile(t3);
 		assertTrue(round.undoMove());
 		assertEquals(0, round.getNumWordsFound());
-		assertTrue(round.moveInProgress.selectedTiles.isEmpty());
+		assertTrue(round.moveInProgress.get().selectedTiles.isEmpty());
 	}
 	
 	/**
@@ -104,7 +104,7 @@ public class TestRound {
 	 */
 	@Test
 	public void testUndoEmptyMove() {
-		Level level = new Level("1");
+		Level level = new Level(6, "1");
 		Round round = new Round(level);
 		assertTrue(round.undoMove());
 		assertEquals(0, round.getNumWordsFound());
@@ -117,7 +117,7 @@ public class TestRound {
 	 */
 	@Test
 	public void testIsOver() {
-		Level level = new Level("1");
+		Level level = new Level(6, "1");
 		Round round = new Round(level);
 		assertEquals(level.isOver(round), round.isOver());
 	}
@@ -127,7 +127,7 @@ public class TestRound {
 	 */
 	@Test
 	public void testIncrementTime() {
-		Level level = new Level("1");
+		Level level = new Level(6, "1");
 		Round round = new Round(level);
 		round.incrementTime();
 		assertEquals(1, round.getTime());
