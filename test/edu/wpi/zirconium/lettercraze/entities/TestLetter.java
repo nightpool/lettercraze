20e17874ab321f23ccabb0f6df765751c48f001a
mlandis <mtlandis@wpi.edu>
2016-12-10 20:50:58 -0500

    Added full coverage tests of Letter and Point

---
+++ test/edu/wpi/zirconium/lettercraze/entities/TestLetter.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package edu.wpi.zirconium.lettercraze.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Mt Landis
+ *
+ */
+public class TestLetter {
+
+	private Letter letter;
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		letter = new Letter("B",4); // TODO should not need to specify score
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		letter = null;
+		assertNull(letter);
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Letter#Letter(java.lang.String, int)}.
+	 */
+	@Test
+	public void testLetter() {
+		assertEquals("B", letter.getCharacter());
+		assertEquals(4, letter.getScore());
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Letter#getScore()}.
+	 */
+	@Test
+	public void testGetScore() {
+		assertEquals(4, letter.getScore());
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Letter#getCharacter()}.
+	 */
+	@Test
+	public void testGetCharacter() {
+		char test1 = 2;
+		
+		assertEquals("B", letter.getCharacter());
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Letter#random()}.
+	 */
+	@Test
+	public void testRandom() {
+		Letter testLetter;
+				for(int i = 0; i < 26; i++){
+					String testString = Character.toString((char)(65 + i));
+					if (testString.equalsIgnoreCase("Q"))
+						testString = "Qu";
+					do{
+						testLetter = Letter.random();
+					}while (!testLetter.getCharacter().equalsIgnoreCase(testString));
+
+					assertEquals(testString,testLetter.getCharacter());
+				}
+		//try over and over until the letter is the correct one
+	}
+
+}
---
+++ test/edu/wpi/zirconium/lettercraze/entities/TestPoint.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package edu.wpi.zirconium.lettercraze.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Mt Landis
+ *
+ */
+public class TestPoint {
+
+	private Point point;
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		point = new Point(1,2);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		point = null;
+		assertNull(point);
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Point#Point(int, int)}.
+	 */
+	@Test
+	public void testPoint() {
+		assertEquals(1, point.getRow());
+		assertEquals(2, point.getColumn());
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Point#getRow()}.
+	 */
+	@Test
+	public void testGetRow() {
+		assertEquals(1, point.getRow());
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Point#getColumn()}.
+	 */
+	@Test
+	public void testGetColumn() {
+		assertEquals(2, point.getColumn());
+	}
+
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Point#isAdjacent(edu.wpi.zirconium.lettercraze.entities.Point)}.
+	 */
+	@Test
+	public void testIsAdjacentCardinal() {
+		assertTrue(point.isAdjacent(new Point(2,2)));
+		assertTrue(point.isAdjacent(new Point(1,3)));
+
+
+	}
+	@Test
+	public void testIsAdjacentDiagonal() {
+		assertTrue(point.isAdjacent(new Point(2,3)));
+		assertTrue(point.isAdjacent(new Point(0,3)));
+	}
+	@Test
+	public void testIsAdjacentOnlyRow() {
+		assertFalse(point.isAdjacent(new Point(2,4)));
+	}
+	@Test
+	public void testIsAdjacentOnlyColumn() {
+		assertFalse(point.isAdjacent(new Point(3,3)));
+	}
+	
+	/**
+	 * Test method for {@link edu.wpi.zirconium.lettercraze.entities.Point#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		assertEquals("Point{" + "row=" + 1 + ", column=" + 2 + '}',point.toString());
+	}
+
+}
