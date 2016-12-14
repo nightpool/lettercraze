76bf8740cf52d2a9f3ff8ec48688026cf268974a
Chris Bove <bove.3123@gmail.com>
2016-12-14 09:55:32 -0500

    WIP on builder controllers

--- src/edu/wpi/zirconium/lettercraze/builder/controllers/BuilderControllers.java
+++ src/edu/wpi/zirconium/lettercraze/builder/controllers/BuilderControllers.java
@@ -1,7 +1,15 @@
 package edu.wpi.zirconium.lettercraze.builder.controllers;
 
 import edu.wpi.zirconium.lettercraze.builder.LetterCrazeBuilder;
+import edu.wpi.zirconium.lettercraze.entities.Board;
+import edu.wpi.zirconium.lettercraze.entities.Level;
+import edu.wpi.zirconium.lettercraze.entities.LevelShape;
+import edu.wpi.zirconium.lettercraze.entities.Tile;
+import edu.wpi.zirconium.lettercraze.player.controllers.SelectTileController;
 import edu.wpi.zirconium.lettercraze.shared.views.BoardView;
+import edu.wpi.zirconium.lettercraze.shared.views.TileView;
+import javafx.beans.binding.Bindings;
+import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -19,11 +27,47 @@ public class BuilderControllers implements Initializable {
     @FXML private Button previewButton;
 
     @FXML private BoardView board;
+    
+    private Board realBoard;
+    private Level level;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         backButton.setOnMouseClicked(_me -> LetterCrazeBuilder.showMenuScreen());
 
         board.getTiles().forEach(t -> t.setOnMouseClicked(c -> t.toggleBlocked()));
+        
+        realBoard = new Board(new LevelShape(6));
+        
+        realBoard.observableTiles().addListener(
+                (ListChangeListener<? super Tile>) l -> {
+                    while (l.next()) {
+                        for (Tile t : l.getAddedSubList()) {
+                            TileView v = board.newTile(t.getPos());
+                            this.bindTile(v, t);
+                            board.getTiles().add(v);
+                        }
+
+                        for (Tile t : l.getRemoved()) {
+                            board.getTiles()
+                                 .removeIf(v -> v.getPos().equals(t.getPos()));
+                        }
+                    }
+                }
+            );
+    }
+    
+    private void bindTile(TileView v, Tile t) {
+        v.valueProperty().set(t.getLetter().getCharacter());
+//        currentRound.moveInProgressProperty().addListener((_m, _o, newMove) -> {
+//            v.selectedProperty().bind(Bindings.createBooleanBinding(
+//                () -> newMove.getSelectedTiles().contains(t),
+//                newMove.getSelectedTiles()));
+//        });
+        t.positionProperty().addListener((_p, oldP, newP) -> {
+            v.setRow(newP.getRow());
+            v.setColumn(newP.getColumn());
+        });
+        v.setOnMouseClicked(new EnableTileController(realBoard, t, v));
     }
 }
