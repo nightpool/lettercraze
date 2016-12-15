53bb795e32a9b52e854e6ba294ce38c972e48fd9
evan <eg1290@gmail.com>
2016-12-15 15:55:04 -0500

    Clean up jar stuff

--- src/edu/wpi/zirconium/lettercraze/utils/WordTable.java
+++ src/edu/wpi/zirconium/lettercraze/utils/WordTable.java
@@ -1,49 +1,52 @@
 package edu.wpi.zirconium.lettercraze.utils;
 
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Hashtable;
-import java.util.Iterator;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
- * Implementation of a word table to lookup words in a dictionary.
+ * Implementation of a word map to lookup words in a dictionary.
  * <p>
  * This package was imported from Professor Heineman's Lecture package.
  * @author Professor Heineman
  *
  */
 public class WordTable {
-	static Hashtable<String,Boolean> table;
+	static Set<String> words;
 	
-	/** Default word table. */
+	/** Default word map. */
 	public static final String wordTable = "resources/WordTable.sort";
 	
 	/**
-	 * Load up word table. Note that there may be superfluous spaces throughout for formatting
-	 * reasons, and these are excised before being added to the table.
+	 * Load up word map. Note that there may be superfluous spaces throughout for formatting
+	 * reasons, and these are excised before being added to the map.
 	 * 
 	 * @throws IOException   if unable to find file 
 	 */
 	public static void loadWordTable() throws IOException {
-		table = new Hashtable<String,Boolean>();
-		
-		Iterator<String> it = new StringFileIterator(new File (".", wordTable));
-		while (it.hasNext()) {
-			String word = it.next();
-			word = word.trim();
-			table.put(word, Boolean.TRUE);
+		words = new HashSet<>(160_232);
+
+		InputStream resourceAsStream = WordTable.class.getResourceAsStream("/WordTable.sort");
+		BufferedReader b = new BufferedReader(new InputStreamReader(resourceAsStream));
+		String line = b.readLine();
+		while (line != null) {
+			words.add(line.trim().toLowerCase());
+			line = b.readLine();
 		}
 	}
 	
 	/**
-	 * Converts word to lowercase and checks whether exists within table.
+	 * Converts word to lowercase and checks whether exists within map.
 	 * 
 	 * @param s
-	 * @return     <code>true</code> if a word in the table; <code>false</code> otherwise.
+	 * @return     <code>true</code> if a word in the map; <code>false</code> otherwise.
 	 */
 	public static boolean isWord(String s) {
-		if (table == null) {
+		if (words == null) {
 			try {
 				loadWordTable();
 			} catch (IOException ioe) {
@@ -51,8 +54,7 @@ public class WordTable {
 				return false;
 			}
 		}
-		s = s.toLowerCase();
-		return table.containsKey(s);
+		return words.contains(s.toLowerCase());
 	}
 }
 
